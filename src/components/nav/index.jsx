dbd36b3d9d290366528ae57331ab5ae8876f06a2
lucasreynoso <lucassreynoso4@gmail.com>
2022-12-23 01:52:48 -0300

    agregado

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import "./styles.css";
 
+
 const Navbar = ({children}) => {
     return(
-        <div className="navbar">
-            <div>
-        <h2>FAMILY burger</h2>
+        <header>
+        <nav class="navbar navbar-expand-lg light colorHeader">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"
+            ><img src="" class="img-fluid" width="100px" alt="logo"
+          /></a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+              <a class="nav-link" href="htmlpages/productos.html">Productos</a>
+              <a class="nav-link" href="htmlpages/nosotros.html">Sobre nosotros</a>
+              <a class="nav-link" href="htmlpages/locales.html">Locales</a>
+              <a class="nav-link" href="htmlpages/contactos.html">Contactos </a>
             </div>
-            {children}
+          </div>
         </div>
+      </nav>
+      </header>
 
     )
 }
