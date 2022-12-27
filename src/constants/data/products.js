01d1891765c181dcad82d88db87d812e10ab2e36
lucasreynoso <lucassreynoso4@gmail.com>
2022-12-27 04:07:01 -0300

    no se

---
+++ src/constants/data/products.js
@@ -0,0 +1,57 @@
+const PRODUCTS = [
+    {
+        id: 1,
+        image: "../img/hamburguesaalbum.jpg",
+        name: "Hamburgesa NN",
+        price: 1300,
+    },
+    {
+        id: 2,
+        image: "../img/pizzaalbum.jpg",
+        name: "Pizza jamon y morron",
+        price: 1000,
+    
+    
+    },
+    {
+        id: 3,
+        image: "../img/empanadas.jpg",
+        name: "Docena de empanadas carne,jyq y pollo",
+        price: 900,
+    },
+    {
+        id:4,
+        image: "../img/pastasalbum.png",
+        name: "pastas",
+        price: 1000,
+    },
+    {
+        id:5,
+        image: "../img/tartasegunda.png",
+        name: "tartas",
+        price: 950
+    },
+    {
+        id:6,
+        image: "../img/postresalbum.png",
+        name: "postres",
+        price: 600
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
